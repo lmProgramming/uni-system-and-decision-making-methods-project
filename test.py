a72a43f25fa05fe51fa1e9c85dcf2e39176b9464
lmProgramming <mikikubs@gmail.com>
2024-05-09 20:07:14 +0200

    pushing data and helpful scripts

---
+++ test.py
@@ -0,0 +1,34 @@
+import numpy as np
+import math
+import random
+
+def custom_logistic(x, mn, mx, mean):
+    a = -4*mean+2
+    
+    m_inter = (mean-mn)/(mx-mn)
+       
+    return x**math.log(m_inter, 0.5) * (mx-mn) + mn
+
+mn = 20
+mx = 100
+mean = 40
+k = 5
+
+mnx = (mean - mn) / (mx - mn)
+
+x_values = np.linspace(0.001, 1, 1000)
+y_values = custom_logistic(x_values, mn, mx, mean)
+
+suma = 0
+for i in range(1000):
+    suma += custom_logistic(random.uniform(0., 1.), mn, mx, mean)
+print(suma / 1000)
+
+import matplotlib.pyplot as plt
+
+plt.plot(x_values, y_values)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Custom Logistic Function')
+plt.grid(True)
+plt.show()
